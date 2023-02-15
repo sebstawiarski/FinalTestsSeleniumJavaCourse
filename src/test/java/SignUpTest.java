1bcead20d836f10000b9bfa9b8430c3ca54861df
sebstawiarski <seb.stawiarski@outlook.com>
2023-02-15 16:34:07 +0100

    Add SignUp test with random email

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -21,6 +21,9 @@ public class SignUpTest {
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
 
+        int randomNumber = (int) (Math.random()*1000);
+        String email = "tester" + randomNumber + "@tester.pl";
+
         driver.findElements(By.xpath("//li[@id='li_myaccount']"))
                 .stream()
                 .filter(WebElement::isDisplayed)
@@ -28,5 +31,20 @@ public class SignUpTest {
                 .ifPresent(WebElement::click);
 
         driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+        String lastName = "Stawiarski";
+
+        driver.findElement(By.name("firstname")).sendKeys("Sebastian");
+        driver.findElement(By.name("lastname")).sendKeys(lastName);
+        driver.findElement(By.name("phone")).sendKeys("+48 333 444 555");
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("Qwerty1!");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Qwerty1!");
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
+
+        Assert.assertTrue(heading.getText().contains(lastName));
+        Assert.assertEquals(heading.getText(),"Hi, Sebastian Stawiarski");
     }
 }
